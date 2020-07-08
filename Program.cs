a030cce6dcc37e3563887e72a25a5871af7d5969
Inn <aliinaan0@gmail.com>
2020-07-08 13:38:33 +0500

    Fixed null error in the original code
    Prevented crash caused by int phase trying to phase a null value

--- Program.cs
+++ Program.cs
@@ -5,7 +5,12 @@ public class Program
 	public static void Main()
 	{
 		var timevalue = 1030; //time on screen without :
-		var password = Math.Pow((9999 - timevalue), 2); //big brain math
+		If(timevalue == null)
+{Console.Writeline("invalid input")}
+else
+{
+var password = Math.Pow((9999 - timevalue), 2); //big brain math
 		Console.WriteLine(password); // output
+}
 	}
 }
