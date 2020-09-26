10ed6539c2e8b045ef751e1d88f3cbcb96358244
Inn <aliinaan0@gmail.com>
2020-09-26 13:04:36 +0500

    Fixed missing ;
    fixed bug
    formatted the code

--- Program.cs
+++ Program.cs
@@ -1,16 +1,16 @@
 using System;
 
-public class Program
-{
-	public static void Main()
-	{
-		var timevalue = 1030; //time on screen without :
-		If(timevalue == null)
-{Console.Writeline("invalid input")}
-else
-{
-var password = Math.Pow((9999 - timevalue), 2); //big brain math
-		Console.WriteLine(password); // output
-}
-	}
+public class Program {
+  public static void Main() {
+    var timevalue = 1030; //time on screen without :
+    If(timevalue == null) 
+    {
+      Console.Writeline("invalid input");
+    }
+    else 
+    {
+      var password = Math.Pow((9999 - timevalue), 2); //big brain math
+      Console.WriteLine(password); // output
+    }
+  }
 }
